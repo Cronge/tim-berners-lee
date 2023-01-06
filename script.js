38a93faecb1ba6852f01ab05a0dae48fe147ab80
cronge7 <107672184+Cronge@users.noreply.github.com>
2023-01-06 18:31:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = () => {
+    scrollFunction()
+};
+
+
+const scrollFunction = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("nav-bar").style.top = "0";
+    } else {
+        document.getElementById("nav-bar").style.top = "-100px";
+    }
+}
